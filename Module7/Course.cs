e9b3d23212dab1b6e87d7271d44356a21d596837
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-05-04 14:03:01 -0500

    Module7 Assignment

---
+++ Module7/Course.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Module7
+{
+    public class Course
+    {
+        private string name;
+        private int credits;
+        private ArrayList students;
+        private ArrayList teachers;
+
+        public Course(string name, int credits=0, ArrayList students=null, ArrayList teachers=null)
+        {
+            this.name = name;
+            this.credits = credits;
+            this.students = students;
+            this.teachers = teachers;
+        }
+
+        public int Credits
+        {
+            get { return credits; }
+            set { credits = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public ArrayList Students
+        {
+            get { return students; }
+            set { students = value; }
+        }
+
+        public ArrayList Teachers
+        {
+            get { return teachers; }
+            set { teachers = value; }
+        }
+
+        public void ListStudents()
+        {
+            foreach (Student student in this.students)
+            {
+                Console.WriteLine("{0} {1}", student.First_name, student.Last_name);
+            }
+        }
+    }
+}
---
+++ Module7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Module7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Santiago", "Mendoza", "Manga", 21);
+            Student student2 = new Student("Ruben", "Melo", "Torices", 23);
+            Student student3 = new Student("Cesar", "De la Hoz", "Cartagena", 24);
+
+            student1.Grades.Push(5);
+            student1.Grades.Push(4);
+            student1.Grades.Push(3);
+            student1.Grades.Push(4);
+            student1.Grades.Push(5);
+            student2.Grades.Push(4);
+            student2.Grades.Push(2);
+            student2.Grades.Push(3);
+            student2.Grades.Push(2);
+            student2.Grades.Push(4);
+            student3.Grades.Push(3);
+            student3.Grades.Push(2);
+            student3.Grades.Push(1);
+            student3.Grades.Push(1);
+            student3.Grades.Push(1);
+
+            Course course_c_sharp = new Course("Programming with C#", 4);
+
+            ArrayList students = new ArrayList();
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+
+            // 3. Add your three students to this Course object.
+            course_c_sharp.Students = students; 
+
+            // 4. Instantiate at least one Teacher object.
+            Teacher teacher = new Teacher("Jairo", "Serrano", "Ternera", 30);
+            ArrayList teachers = new ArrayList();
+
+            teachers.Add(teacher);
+
+            // 5. Add that Teacher object to your Course object
+            course_c_sharp.Teachers = teachers;
+
+            course_c_sharp.ListStudents();
+            Console.ReadKey();
+        }
+    }
+}
